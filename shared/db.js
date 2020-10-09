8bd79c96479fb38f4d6e7f023efb96a9d7e5dcde
Glitch (scythe-phrygian-talk) <none>
2020-10-09 13:59:49 +0000

    ⛱🍦 Checkpoint ./shared/db.js:22724841/1508

--- shared/db.js
+++ shared/db.js
@@ -1,8 +1,34 @@
-const low = require('lowdb');
+/*const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('./shared/db.json');
 const db = low(adapter);
 
-db.defaults({ books: [], users: [], trans: [] }).write();
+db.defaults({ books: [], users: [], trans: [] }).write();*/
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const UserSchema = new Schema({
+  _id: ObjectId,
+  username: String,
+  email: String,
+  password: String,
+  isAdmin: Number,
+  wrongLoginCount: Number,
+  avatarUrl: String,
+});
+
+const BookSchema = new Schema({
+  _id: ObjectId,
+  title: String,
+  description: String
+});
+
+const TranSchema = new Schema({
+  _id: ObjectId,
+  userId: String,
+  bookId: String,
+  isComplete: Boolean
+});
 
 module.exports = db;
