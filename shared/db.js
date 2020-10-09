8a84cd536710cc5f4dcc846ea5b541a040aedc84
Glitch (scythe-phrygian-talk) <none>
2020-10-09 14:11:32 +0000

    😬📒 Checkpoint ./shared/db.js:22724841/734

--- shared/db.js
+++ shared/db.js
@@ -5,11 +5,12 @@ const db = low(adapter);
 
 db.defaults({ books: [], users: [], trans: [] }).write();*/
 
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
+//const ObjectId = Schema.ObjectId;
 
 const UserSchema = new Schema({
-  _id: ObjectId,
+  //_id: ObjectId,
   username: String,
   email: String,
   password: String,
@@ -19,16 +20,32 @@ const UserSchema = new Schema({
 });
 
 const BookSchema = new Schema({
-  _id: ObjectId,
+  //_id: ObjectId,
   title: String,
-  description: String
+  description: String,
+  coverUrl: String,
 });
 
 const TranSchema = new Schema({
-  _id: ObjectId,
+  //_id: ObjectId,
   userId: String,
   bookId: String,
   isComplete: Boolean
 });
 
-module.exports = db;
+const sessionSchema = new Schema({
+  cart: [
+    {
+      bookId: { type: Schema.Types.ObjectId, ref: "Book" },
+      quantity: Number
+    }
+  ]
+});
+
+const Session = mongoose.model("sessions", sessionSchema);
+const books = mongoose.model("books", BookSchema);
+const users = mongoose.model("users", UserSchema);
+const trans = mongoose.model("trans", TranSchema);
+module.exports = (books, users, trans)
+
+//module.exports = db;
