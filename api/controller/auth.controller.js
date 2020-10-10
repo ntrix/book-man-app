dccfd64c2465704b505ee209db792e739aada8e5
Glitch (scythe-phrygian-talk) <none>
2020-10-10 21:26:38 +0000

    👫🛩 Checkpoint ./api/controller/auth.controller.js:22724841/782

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -8,47 +8,51 @@ const { User } = require("../../shared/db");
 module.exports.postLogin = async (req, res, next) => {
   const errors = res.locals.errors || [];
   const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email: email });
   
-  const user = await User.findOne({ email: email });
-  if (!user) return;
-  let count = await user.wrongLoginCount || 0;
-  
-  if (count == 4)
-    errors.push("Too many fail attempts! Please try again in 24 hours or reset your password.");
-  else if (!user)
-    errors.push("User does not exist!")
-  else if (password && !bcrypt.compareSync(password, user._doc.password)) {
-    user.wrongLoginCount = ++count;
-    user.save( err => err? console.log(err) :0);
-    errors.push("Wrong password! " + count + " of 4 attempts.");
-    
-    if (count >= 3) {
-      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-      const msg = {
-        to: email,
-        from: 'dev@tienguyen.com',
-        subject: 'Warning too many failed attempts by logging in',
-        text: 'You have reached ' + count + ' of 4 attempts to login. Please be careful or your account will be locked for 24 hours.',
-        html: '<strong>You can also reset your password</strong>',
-      };
-      try {
-        await sgMail.send(msg);
-      } catch (err) {
-        console.log(err);
+    let count = user? user.wrongLoginCount || 0;
+
+    if (count == 4)
+      errors.push("Too many fail attempts! Please try again in 24 hours or reset your password.");
+    else if (!user)
+      errors.push("User does not exist!")
+    else if (password && !bcrypt.compareSync(password, user._doc.password)) {
+      user.wrongLoginCount = ++count;
+      user.save( err => err? console.log(err) :0);
+      errors.push("Wrong password! " + count + " of 4 attempts.");
+
+      if (count >= 3) {
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        const msg = {
+          to: email,
+          from: 'dev@tienguyen.com',
+          subject: 'Warning too many failed attempts by logging in',
+          text: 'You have reached ' + count + ' of 4 attempts to login. Please be careful or your account will be locked for 24 hours.',
+          html: '<strong>You can also reset your password</strong>',
+        };
+        try {
+          await sgMail.send(msg);
+        } catch (err) {
+          console.log(err);
+        }
+        errors.push("A warning message has been sent to this email address!");
       }
-      errors.push("A warning message has been sent to this email address!");
     }
-  }
 
-  if (errors.length) {
-    res.json({ errors: errors });
-    return;
-  }
+    if (errors.length) {
+      res.json({ errors: errors });
+      return;
+    }
 
-  user.wrongLoginCount = 0;
-  user.save( err => err? console.log(err) :0);
-  
-  res.cookie('userId', user._id, { signed: true });
-  res.cookie('isAdmin', user.isAdmin || 0, { signed: true });
-  res.json({ token: res.cookie });
+    user.wrongLoginCount = 0;
+    user.save( err => err? console.log(err) :0);
+
+    res.cookie('userId', user._id, { signed: true });
+    res.cookie('isAdmin', user.isAdmin || 0, { signed: true });
+    res.json({ token: res.cookie });
+  } catch(e) {
+    console.log('user not found: ', e);
+    next();
+  }
 }
