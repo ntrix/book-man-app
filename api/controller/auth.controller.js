4f027e1d2f9997908053d776afdb6f4203e216cd
Glitch (scythe-phrygian-talk) <none>
2020-10-10 21:15:20 +0000

    🙋🏁 Checkpoint ./api/controller/auth.controller.js:22724841/39

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -10,8 +10,8 @@ module.exports.postLogin = async (req, res, next) => {
   const { email, password } = req.body;
   
   const user = await User.findOne({ email: email });
-  debugger;
-  let count = user.wrongLoginCount || 0;
+  if (!user) return;
+  let count = await user.wrongLoginCount || 0;
   
   if (count == 4)
     errors.push("Too many fail attempts! Please try again in 24 hours or reset your password.");
