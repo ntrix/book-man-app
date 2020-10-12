9fdeaa0ca29c09c30857de0bfb11e852606a81dd
ntrix <ntrix@ntien.com>
2020-10-12 22:53:42 +0200

    fixed: authen. user management for admin only

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,16 +1,17 @@
 module.exports = {
-  requireAuth: (req, res, next) => {
-    if (!req.signedCookies.userId) {
-      res.cookie('path', req.baseUrl, { signed: true });
-      res.redirect('/auth/login');
-      return;
-    }
-    next();
-  },
-  
-  isAdmin: (req, res, next) => {
-    if (req.signedCookies.isAdmin)
-      next();
-  }
-  
-}
+	requireAuth: (req, res, next) => {
+		if (!req.signedCookies.userId) {
+			res.cookie("path", req.baseUrl, { signed: true });
+			res.redirect("/auth/login");
+			return;
+		}
+		next();
+	},
+
+	isAdmin: (req, res, next) => {
+		console.log("admin: ", req.signedCookies.isAdmin);
+		if (req.signedCookies.isAdmin == 1) return next();
+		res.render("index", { errors: "for admin only" });
+		console.error("only for admin");
+	},
+};
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,20 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const userController = require('../controller/user.controller')
-const validate = require('../validate/user.validate');
-const authMiddleware = require('../middleware/auth.middleware');
+const userController = require("../controller/user.controller");
+const validate = require("../validate/user.validate");
+const authMiddleware = require("../middleware/auth.middleware");
 
-router.use(authMiddleware.requireAuth);
+router.use(authMiddleware.requireAuth, authMiddleware.isAdmin);
 
-router.post('/add', validate.postAdd, userController.postAdd);
+router.post("/add", validate.postAdd, userController.postAdd);
 
-router.get('/', authMiddleware.isAdmin, userController.index);
+router.get("/", userController.index);
 
-router.get('/upd/:id', authMiddleware.isAdmin, userController.update);
+router.get("/upd/:id", userController.update);
 
-router.post('/upd',  validate.postAdd, userController.postUpdate);
+router.post("/upd", validate.postAdd, userController.postUpdate);
 
-router.get('/del/:id', authMiddleware.isAdmin, userController.delete);
+router.get("/del/:id", userController.delete);
 
 module.exports = router;
